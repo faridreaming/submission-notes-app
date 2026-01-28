f78e01dedb28f7e31727d0cc218fa52699075000
faridreaming <97384053+faridreaming@users.noreply.github.com>
2026-01-28 15:25:15 +0700

    feat: create header-bar component

---
+++ js/components/header-bar.js
@@ -0,0 +1,47 @@
+class HeaderBar extends HTMLElement {
+  constructor() {
+    super()
+    this.attachShadow({ mode: 'open' })
+  }
+
+  connectedCallback() {
+    this.render()
+  }
+
+  render() {
+    this.shadowRoot.innerHTML = `
+      <style>
+        div {
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+          margin-block-start: 1rem;
+          div {
+            background-color: oklch(53.2% 0.157 131.589);
+            width: fit-content;
+            height: fit-content;
+            padding: 1rem;
+            display: flex;
+            border-radius: 50%;
+          }
+          img {
+            height: 3rem;
+            width: 3rem;
+          }
+          h1 {
+            margin: 0;
+          }
+        }
+      </style>
+      <div>
+        <div>
+          <img src="../../assets/logo.svg" alt="Logo" />
+        </div>
+        <h1>Note Apps Farid</h1>
+      </div>
+    `
+  }
+}
+
+customElements.define('header-bar', HeaderBar)
