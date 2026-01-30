ef9914fed3f099c3a28913f0404006176beb9eef
faridreaming <97384053+faridreaming@users.noreply.github.com>
2026-01-31 05:06:30 +0700

    feat: display note list in a simple way

--- app.js
+++ app.js
@@ -1,11 +1,32 @@
 import './js/components/index.js'
+import Notes from './js/notes.js'
 
 class App {
   static instance = null
 
   constructor() {
     if (App.instance) return App.instance
+
+    this.initElements()
+    this.displayNotes()
+
     App.instance = this
+  }
+
+  initElements() {
+    this.noteListElement = document.querySelector('note-list')
+  }
+
+  displayNotes() {
+    const notes = Notes.getAll()
+    const noteItemElements = notes.map((note) => {
+      const noteItemElement = document.createElement('note-item')
+      noteItemElement.note = note
+      return noteItemElement
+    })
+
+    this.noteListElement.innerHTML = ''
+    this.noteListElement.append(...noteItemElements)
   }
 }
 
