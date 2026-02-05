71f19ae892fb962f475d0e0ae3087977b5be73b0
faridreaming <97384053+faridreaming@users.noreply.github.com>
2026-02-05 17:13:21 +0700

    feat: create toast-message component

---
+++ js/components/toast-message.js
@@ -0,0 +1,48 @@
+class ToastMessage extends HTMLElement {
+  static get observedAttributes() {
+    return ['message']
+  }
+
+  constructor() {
+    super()
+    this.message = this.getAttribute('message') || ''
+  }
+
+  connectedCallback() {
+    this.render()
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    if (oldValue !== newValue) {
+      this[name] = newValue
+      this.render()
+    }
+  }
+
+  render() {
+    this.innerHTML = `
+      <style>
+        toast-message {
+          display: block;
+        }
+        
+        .toast {
+          padding: 0.5rem 1rem;
+          border-radius: 0.5rem;
+          width: fit-content;
+          color: oklch(14.1% 0.005 285.823);
+          background-color: oklch(98.5% 0 0);
+          position: fixed;
+          right: 1rem;
+          bottom: -100%;
+          transition: all 0.3s ease-in-out;
+        }
+      </style>
+      <div class="toast">
+        ${this.message}
+      </div>
+    `
+  }
+}
+
+customElements.define('toast-message', ToastMessage)
